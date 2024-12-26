ae3eb41849979c2871a7acc9b4751621883c7fca
bbhoom <bhoomika432004@gmail.com>
2024-12-26 20:41:54 +0530

    Initial commit after reconnecting

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { ghPages } from 'vite-plugin-gh-pages';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/Bhoomika-Portfolio/',
+
+});
+
+
+
